1095a01fc8288dd23f2c5c9f9f8bb575c754f49f
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2024-08-11 17:06:59 +0000

    added images

--- script.js
+++ script.js
@@ -4,27 +4,45 @@ let apiurl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
 
 const inputBtn = document.querySelector(".input");
 const searchBtn = document.querySelector('.btn');
+const weatherIcon = document.querySelector('.weather-img')
 const city = inputBtn.value;
+
 searchBtn.addEventListener('click', () => {
     searchCity(inputBtn.value)
 });
 
-if(inputBtn.value===''){
-    alert('write a city name');
-}
-else
-{
-    async function searchCity(city){
 
-        const response = await fetch(apiurl + city + `&appid=${apikey}`);
-        const data = await response.json();
-        console.log(data)
+async function searchCity(city) {
+
+    const response = await fetch(apiurl + city + `&appid=${apikey}`);
+    const data = await response.json();
+    console.log(data)
+
+    document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector('.city-name').innerHTML = data.name;
+    document.querySelector('.humi-level').innerHTML = data.main.humidity + "%";
+    document.querySelector('.wind-level').innerHTML = data.wind.speed + "km/h";
+
     
-        document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + "°C";
-        document.querySelector('.city-name').innerHTML = data.name;
-        document.querySelector('.humi-level').innerHTML = data.main.humidity + "%";
-        document.querySelector('.wind-level').innerHTML = data.wind.speed + "km/h";
-    }
-    
+if (data.weather[0].main == "Rain") {
+    weatherIcon.src = "images/rain.png"
 }
+else if(data.weather[0].main == "Clouds"){
+     weatherIcon.src = 'images/clouds.png'
+}
+else if(data.weather[0].main == "Clear"){
+    weatherIcon.src = 'images/clear.png'
+}
+else if(data.weather[0].main == "Drizzel"){
+    weatherIcon.src = 'images/drizzel.png'
+}
+else if(data.weather[0].main == "Snow"){
+    weatherIcon.src = 'images/snow.png'
+}
+else if(data.weather[0].main == "Mist"){
+    weatherIcon.src = 'images/mist.png'
+}
+
+}
+
 
